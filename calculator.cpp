9aaa7b28ab378ded761186ecc83e71fb3e44859b
Blazej Leman <blazejleman789@gmail.com>
2023-12-25 20:33:06 +0100

    test_v0.1

---
+++ calculator.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+
+
+
+std::string enter_number() {
+    std::string text;
+    std::cout << "number format e.g.: x + y" << std::endl;
+    std::cin >> text;
+    return text;
+}
+
+std::string start() {
+    std::string text = enter_number();
+    std::cout << text;
+}
+
+int main() {
+    start();
+}
