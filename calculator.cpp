9adb217c7123040909ff667cf5e8eb22e45c59e2
Blazej Leman <blazejleman789@gmail.com>
2023-12-25 21:56:09 +0100

    calculator_v0.3

--- calculator.cpp
+++ calculator.cpp
@@ -4,8 +4,47 @@
 std::string enter_number() {
     std::string text;
     std::cout << "number format e.g.: 'x + y'" << std::endl;
+    std::cout << "allowed operations: +/-/*//" << std::endl;
     std::getline(std::cin,text);
     return text;
+}
+
+int sign_fun(std::string sign) {
+    std::string sign_re = sign;
+    if(sign_re == "+") {
+        return 1;
+    } else if(sign_re == "-") {
+        return 2;
+    } else if(sign_re == "*") {
+        return 3;
+    } else if (sign_re == "/") {
+        return 4;
+    }
+}
+
+int simple_algorithms(int x, std::string sign, int y) {
+    int x_re = x;
+    int y_re = y;
+    switch (sign_fun(sign))
+    {
+    case 1:
+        std::cout << x_re + y_re;
+        break;
+    case 2:
+        std::cout << x_re - y_re;
+        break;
+    case 3:
+        std::cout << x_re * y_re;
+        break;
+    case 4:
+        if(y_re == 0) {
+            std::cout << "cannot be divided by zero" << std::endl;
+        } else {
+            std::cout << x_re / y_re;
+        }
+        break;
+    return 0;
+    }
 }
 
 int converter(std::string x, std::string sign, std::string y) {
@@ -19,6 +58,7 @@ int converter(std::string x, std::string sign, std::string y) {
     int y_con = std::stoi(y);
     std::cout << "y =" << y_con << ":" << std::endl;
 
+    simple_algorithms(x_con, sign_con, y_con);
     return 0;
 }
 
