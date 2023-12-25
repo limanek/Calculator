79800f1b7ccea7e50e3a018f38252c4b43c01b2a
Blazej Leman <blazejleman789@gmail.com>
2023-12-25 20:51:32 +0100

    calculator_v0.1.2

--- calculator.cpp
+++ calculator.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 #include <string>
 
-
-
-
 std::string enter_number() {
     std::string text;
-    std::cout << "number format e.g.: x + y" << std::endl;
+    std::cout << 'number format e.g.:"x + y"' << std::endl;
     std::getline(std::cin,text);
     return text;
 }
 
-std::string start() {
+int separation(std::string text) {
+    std::string text = text;
+    std::string x = text.erase(0, 1);
+    std::string sign = text.erase(2, 3);
+    std::string y = text.erase(3, 4);
+    std::cout << x << " " << sign << " " << y << std::endl;
+}
+
+int start() {
     std::string text = enter_number();
-    std::cout << text;
+    std::string separation(text);
 }
 
 int main() {
